690868ab8908b16d8c34417c318f663741242f8f
Nikita Tchayka <nick@booster.cloud>
2021-03-10 22:45:48 +0000

    wip: Elm architecture

--- src/soup_machinepkg/device_test.nim
+++ src/soup_machinepkg/device_test.nim
@@ -1,11 +1,46 @@
 import ./cabl
 from strformat import fmt
+import patty
+import locks
+
+
+# variant Node:
+#   Text(text: string)
+
+# type AppSignal[TAction] = proc(action: TAction): (proc(): Node)
+
+# type AppConfig[TState, TAction] = object
+#   initial*: TState
+#   update*: proc (model: TState, action: TAction): TState
+#   view*: proc(model: TState, signal: AppSignal): Node
+
+
+# proc app[TState, TAction](config: AppConfig[TState, TAction]): void =
+#   var model = config.initial
+#   var signal: AppSignal
+#   signal =
+#     proc (action: TAction): proc  =
+#       return proc () =
+#         model = config.update(model, action)
+#         config.view(signal, model)
+#   config.view(signal, model)
+#   echo "Running app"
+
+variant Event:
+  ButtonChanged(bcButton: DeviceButton, bcState: bool, bcShift: bool)
+
+var
+  modelLock: Lock
+  eventsChannel: Channel[Event]
+
+
+
+# Device stuff
 
 proc initDevice*(): void =
   echo "Init Device"
 
-proc render*(): void =
-  echo "Render"
+proc render*(): void = discard
 
 proc buttonChanged*(device: DevicePtr, button: DeviceButton, state: bool, shift: bool): void =
   let white = newColor(255)
