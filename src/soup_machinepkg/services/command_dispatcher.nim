3f926bdc0fca4f6e4889583a39b9b569d26d955a
Nikita Tchayka <nick@booster.cloud>
2021-03-14 22:59:09 +0000

    Start experimenting with SuperCollider

--- src/soup_machinepkg/services/command_dispatcher.nim
+++ src/soup_machinepkg/services/command_dispatcher.nim
@@ -18,12 +18,14 @@ proc dispatch(io: IO, cmd: Cmd): void =
       io.device.setKeyLed(index, int(value * 0xff))
     PlaySound(freq, volume):
       # io.sound.playSound()
-      discard execShellCmd "oscsend localhost 5510 /noise/gate f 1"
-      discard execShellCmd fmt"oscsend localhost 5510 /noise/gain f {$volume}"
-      echo fmt"Running  oscsend localhost 5510 /noise/freq f {$freq}"
-      discard execShellCmd fmt"oscsend localhost 5510 /noise/freq f {$freq}"
+      let ip = "192.168.86.34"
+      let port = "7771"
+      # discard execShellCmd "oscsend {ip} {port} /noise/gate f 1"
+      # discard execShellCmd fmt"oscsend {ip} {port} /noise/gain f {$volume}"
+      # discard execShellCmd fmt"oscsend {ip} {port} /noise/freq f {$freq}"
+      discard execShellCmd fmt"oscsend {ip} {port} /ding"
     MuteSound:
-      discard execShellCmd "oscsend localhost 5510 /noise/gate f 0"
+      discard execShellCmd "oscsend {ip} {port} /noise/gate f 0"
     CmdNone:
       discard
 
